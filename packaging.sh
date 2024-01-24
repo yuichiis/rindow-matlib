535a813ce03d84b66642cdd0bb860abac296b920
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-01-24 19:32:30 +0900

    packaging

--- packaging.sh
+++ packaging.sh
@@ -7,6 +7,7 @@ INC_DIR=$INSTALL_DIR/include/rindow
 
 PKG_WORK=pkgwork
 RINDOW_MATLIB_VERSION=`fgrep "#define RINDOW_MATLIB_VERSION" include/rindow/matlib.h | cut -d " " -f 3 | cut -d "\"" -f 2`
+RINDOW_MATLIB_MAJOR_VERSION=`echo %RINDOW_MATLIB_VERSION% | cut -d "." -f 1`
 . /etc/os-release
 OS_VERSION=$ID$VERSION_ID
 echo RINDOW_MATLIB_VERSION=$RINDOW_MATLIB_VERSION
@@ -18,12 +19,14 @@ mkdir -p $PKG_WORK$INC_DIR
 mkdir -p $PKG_WORK/DEBIAN
 cp build$CONFIG_DIR/librindowmatlib.so $PKG_WORK$LIB_DIR/librindowmatlib.$RINDOW_MATLIB_VERSION.so
 chmod 744 $PKG_WORK$LIB_DIR/librindowmatlib.$RINDOW_MATLIB_VERSION.so
+( cd $PKG_WORK$LIB_DIR ; ln -s librindowmatlib.$RINDOW_MATLIB_VERSION.so librindowmatlib.so )
+( cd $PKG_WORK$LIB_DIR ; ln -s librindowmatlib.$RINDOW_MATLIB_VERSION.so librindowmatlib.$RINDOW_MATLIB_MAJOR_VERSION.so )
 cp include/rindow/matlib.h $PKG_WORK$INC_DIR/.
 chmod 444 $PKG_WORK$INC_DIR/matlib.h
 sed -e s/%RINDOW_MATLIB_VERSION%/$RINDOW_MATLIB_VERSION/ debian/control | \
 sed -e s/%OS_VERSION%/$OS_VERSION/ > $PKG_WORK/DEBIAN/control
 sed -e s/%RINDOW_MATLIB_VERSION%/$RINDOW_MATLIB_VERSION/ pkgconfig/rindowmatlib.pc | \
-sed -e s/%OS_VERSION%/$OS_VERSION/ > $PKG_WORK$LIB_DIR/pkgconfig/rindowmatlib.pc
+sed -e s/%INSTALL_DIR%/$INSTALL_DIR/ > $PKG_WORK$LIB_DIR/pkgconfig/rindowmatlib.pc
 #sed -e s@%EXTENSION_DIR%@$EXTENSION_DIR@ debian/rules | \
 #sed -e s@%INI_DIR%@$INI_DIR@ debian/rules | \
 #	> $PKG_WORK/DEBIAN/rules
