1d986e12612de20fac2e448b3892980b17a120fe
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-01-24 15:30:54 +0900

    fix pthred

--- src/sum.c
+++ src/sum.c
@@ -2,8 +2,8 @@
 #include <math.h>
 #include "common.h"
 
-#ifndef _OPENMP
-
+//#ifndef _OPENMP
+/*
 static void *s_sum_kernel(void *varg)
 {
     arg_s_sum_kernel_t *arg=varg;
@@ -34,19 +34,21 @@ static void *d_sum_kernel(void *varg)
 
     return NULL;
 }
-#endif
+*/
+//#endif
 
 float rindow_matlib_s_sum(int32_t n,float *x, int32_t incX, float tsum)
 {
     float sum = 0;
-#ifdef _OPENMP
+//#ifdef _OPENMP
     int32_t i;
     #pragma omp parallel for reduction(+:sum)
     for(i=0; i<n; i++) {
         sum += x[i*incX];
     }
     return tsum+sum;
-#else
+//#else
+/*
     int32_t num_thread = rindow_matlib_common_get_nprocs();
     int32_t cell_size = n/num_thread;
     int32_t remainder = n-cell_size*num_thread;
@@ -72,22 +74,23 @@ float rindow_matlib_s_sum(int32_t n,float *x, int32_t incX, float tsum)
     }
     free(th_arg);
     return tsum+sum;
-#endif
+*/
+//#endif
 }
 
 double rindow_matlib_d_sum(int32_t n,double *x, int32_t incX, double tsum)
 {
     double sum = 0;
-#ifdef _OPENMP
+//#ifdef _OPENMP
     int32_t i;
     #pragma omp parallel for reduction(+:sum)
     for(i=0; i<n; i++) {
         sum += x[i*incX];
     }
     return tsum+sum;
-#else
+//#else
 
-
+/*
     int32_t num_thread = rindow_matlib_common_get_nprocs();
     int32_t cell_size = n/num_thread;
     int32_t remainder = n-cell_size*num_thread;
@@ -113,7 +116,8 @@ double rindow_matlib_d_sum(int32_t n,double *x, int32_t incX, double tsum)
     }
     free(th_arg);
     return tsum+sum;
-#endif
+*/
+//#endif
 }
 
 
