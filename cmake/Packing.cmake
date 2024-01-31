55eded3153203ad94668ad0a6057f29f3538207f
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-02-01 07:22:24 +0900

    linux

--- cmake/Packing.cmake
+++ cmake/Packing.cmake
@@ -30,8 +30,8 @@ set(CPACK_PACKAGE_VERSION_PATCH ${PROJECT_VERSION_PATCH})
 set(CPACK_PACKAGE_CONTACT "Rindow Developers <rindow@users.noreply.github.com>")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "YOUR NAME")
 
-set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/../LICENSE")
-set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/../README.md")
+set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 
 # package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
 # you'll get some-application_0.9.2_amd64.deb (note the underscores too)
