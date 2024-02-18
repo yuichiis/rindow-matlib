5e5ad1cc33375774c964396371a20a34ae1f4ea8
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-02-18 09:56:47 +0900

    reduce argmax max sum test

--- tests/ReduceMaxTest.cpp
+++ tests/ReduceMaxTest.cpp
@@ -3,13 +3,14 @@
 
 #include "rindow/matlib.h"
 #include <numeric>
+#include <limits>
 
 using testing::ContainerEq;
 
 namespace {
 
 template <typename T>
-class ReduceArgmaxTest : public ::testing::Test {
+class ReduceMaxTest : public ::testing::Test {
 protected:
     int32_t calcShape(
         std::initializer_list<int32_t> shape,
@@ -32,35 +33,32 @@ protected:
         return 0;
     }
 
-    virtual void test_matlib_reduceargmax(
+    virtual void test_matlib_reducemax(
         int32_t m,int32_t n,int32_t k,
         float *a,
-        int32_t dtype,
-        void *b
+        float *b
     )
     {
-        return rindow_matlib_s_reduceargmax(m,n,k,a,dtype,b);
+        return rindow_matlib_s_reducemax(m,n,k,a,b);
     }
 
-    virtual void test_matlib_reduceargmax(
+    virtual void test_matlib_reducemax(
         int32_t m,int32_t n,int32_t k,
         double *a,
-        int32_t dtype,
-        void *b
+        double *b
     )
     {
-        return rindow_matlib_d_reduceargmax(m,n,k,a,dtype,b);
+        return rindow_matlib_d_reducemax(m,n,k,a,b);
     }
 };
 typedef ::testing::Types<float, double> TestTypes;
-TYPED_TEST_SUITE(ReduceArgmaxTest, TestTypes);
+TYPED_TEST_SUITE(ReduceMaxTest, TestTypes);
 
-TYPED_TEST(ReduceArgmaxTest, Array2DAxis0) {
+TYPED_TEST(ReduceMaxTest, Array2DAxis0) {
     const int32_t shapeA0 = 2;
     const int32_t shapeA1 = 3;
     const int32_t shapeB = shapeA1;
     const int32_t axis = 0;
-    const int32_t IndexDtype = rindow_matlib_dtype_int32;
     int32_t m;
     int32_t n;
     int32_t k;
@@ -70,7 +68,7 @@ TYPED_TEST(ReduceArgmaxTest, Array2DAxis0) {
         {1,2,3},
         {4,5,6}
     };
-    int32_t B[shapeB];
+    TypeParam B[shapeB];
     this->calcShape({shapeA0,shapeA1},axis,&m,&n,&k,&outputSize);
     
     ASSERT_EQ(1,        m);
@@ -78,21 +76,36 @@ TYPED_TEST(ReduceArgmaxTest, Array2DAxis0) {
     ASSERT_EQ(shapeA1,  k);
     ASSERT_EQ(shapeB,   outputSize);
 
-    this->test_matlib_reduceargmax(
+    this->test_matlib_reducemax(
         m,n,k,
         (TypeParam*)A,
-        IndexDtype,B
+        (TypeParam*)B
     );
 
-    int32_t R1[shapeB] = {1,1,1};
+    TypeParam R1[shapeB] = {4,5,6};
+    EXPECT_THAT(R1, ContainerEq(B));
+
+    //  minus
+    TypeParam A2[shapeA0][shapeA1] = {
+        {-1,-2,-3},
+        {-4,-5,-6}
+    };
+    TypeParam B2[shapeB];
+
+    this->test_matlib_reducemax(
+        m,n,k,
+        (TypeParam*)A2,
+        (TypeParam*)B2
+    );
+
+    TypeParam R2[shapeB] = {-1,-2,-3};
     EXPECT_THAT(R1, ContainerEq(B));
 }
-TYPED_TEST(ReduceArgmaxTest, Array2DAxis1) {
+TYPED_TEST(ReduceMaxTest, Array2DAxis1) {
     const int32_t shapeA0 = 2;
     const int32_t shapeA1 = 3;
     const int32_t shapeB = shapeA0;
     const int32_t axis = 1;
-    const int32_t IndexDtype = rindow_matlib_dtype_int32;
     int32_t m;
     int32_t n;
     int32_t k;
@@ -102,7 +115,7 @@ TYPED_TEST(ReduceArgmaxTest, Array2DAxis1) {
         {1,2,3},
         {4,5,6}
     };
-    int32_t B[shapeB];
+    TypeParam B[shapeB];
     this->calcShape({shapeA0,shapeA1},axis,&m,&n,&k,&outputSize);
     
     ASSERT_EQ(shapeA0,  m);
@@ -110,23 +123,38 @@ TYPED_TEST(ReduceArgmaxTest, Array2DAxis1) {
     ASSERT_EQ(1,        k);
     ASSERT_EQ(shapeB,   outputSize);
 
-    this->test_matlib_reduceargmax(
+    this->test_matlib_reducemax(
         m,n,k,
         (TypeParam*)A,
-        IndexDtype,B
+        (TypeParam*)B
     );
 
-    int32_t R1[shapeB] = {2,2};
+    TypeParam R1[shapeB] = {3,6};
+    EXPECT_THAT(R1, ContainerEq(B));
+
+    //  minus
+    TypeParam A2[shapeA0][shapeA1] = {
+        {-1,-2,-3},
+        {-4,-5,-6}
+    };
+    TypeParam B2[shapeB];
+
+    this->test_matlib_reducemax(
+        m,n,k,
+        (TypeParam*)A2,
+        (TypeParam*)B2
+    );
+
+    TypeParam R2[shapeB] = {-1,-4};
     EXPECT_THAT(R1, ContainerEq(B));
 }
-TYPED_TEST(ReduceArgmaxTest, Array3DAxis0) {
+TYPED_TEST(ReduceMaxTest, Array3DAxis0) {
     const int32_t shapeA0 = 2;
     const int32_t shapeA1 = 2;
     const int32_t shapeA2 = 2;
     const int32_t shapeB0 = shapeA1;
     const int32_t shapeB1 = shapeA2;
     const int32_t axis = 0;
-    const int32_t IndexDtype = rindow_matlib_dtype_int32;
     int32_t m;
     int32_t n;
     int32_t k;
@@ -138,7 +166,7 @@ TYPED_TEST(ReduceArgmaxTest, Array3DAxis0) {
         {{5,6},
          {7,8}}
     };
-    int32_t B[shapeB0][shapeB1];
+    TypeParam B[shapeB0][shapeB1];
     this->calcShape({shapeA0,shapeA1,shapeA2},axis,&m,&n,&k,&outputSize);
     
     ASSERT_EQ(1,                m);
@@ -146,23 +174,22 @@ TYPED_TEST(ReduceArgmaxTest, Array3DAxis0) {
     ASSERT_EQ(shapeA1*shapeA2,  k);
     ASSERT_EQ(shapeB0*shapeB1,  outputSize);
 
-    this->test_matlib_reduceargmax(
+    this->test_matlib_reducemax(
         m,n,k,
         (TypeParam*)A,
-        IndexDtype,(int32_t*)B
+        (TypeParam*)B
     );
 
-    int32_t R1[shapeB0][shapeB1] = {{1,1},{1,1}};
+    TypeParam R1[shapeB0][shapeB1] = {{5,6},{7,8}};
     EXPECT_THAT(R1, ContainerEq(B));
 }
-TYPED_TEST(ReduceArgmaxTest, Array3DAxis1) {
+TYPED_TEST(ReduceMaxTest, Array3DAxis1) {
     const int32_t shapeA0 = 3;
     const int32_t shapeA1 = 2;
     const int32_t shapeA2 = 4;
     const int32_t shapeB0 = shapeA0;
     const int32_t shapeB1 = shapeA2;
     const int32_t axis = 1;
-    const int32_t IndexDtype = rindow_matlib_dtype_int32;
     int32_t m;
     int32_t n;
     int32_t k;
@@ -176,7 +203,7 @@ TYPED_TEST(ReduceArgmaxTest, Array3DAxis1) {
         {{1,4,5,8},
          {2,3,6,7}},
     };
-    int32_t B[shapeB0][shapeB1];
+    TypeParam B[shapeB0][shapeB1];
     this->calcShape({shapeA0,shapeA1,shapeA2},axis,&m,&n,&k,&outputSize);
     
     ASSERT_EQ(shapeA0,          m);
@@ -184,18 +211,58 @@ TYPED_TEST(ReduceArgmaxTest, Array3DAxis1) {
     ASSERT_EQ(shapeA2,          k);
     ASSERT_EQ(shapeB0*shapeB1,  outputSize);
 
-    this->test_matlib_reduceargmax(
+    this->test_matlib_reducemax(
         m,n,k,
         (TypeParam*)A,
-        IndexDtype,(int32_t*)B
+        (TypeParam*)B
     );
 
-    int32_t R1[shapeB0][shapeB1] = {
-        {1,0,1,0},
-        {1,0,1,0},
-        {1,0,1,0},
+    TypeParam R1[shapeB0][shapeB1] = {
+        {2,4,6,8},
+        {2,4,6,8},
+        {2,4,6,8}
     };
     EXPECT_THAT(R1, ContainerEq(B));
+}
+TYPED_TEST(ReduceMaxTest, Array2DAxis1WithNaN) {
+    const int32_t shapeA0 = 6;
+    const int32_t shapeA1 = 2;
+    const int32_t shapeB = shapeA0;
+    const int32_t axis = 1;
+    int32_t m;
+    int32_t n;
+    int32_t k;
+    int32_t outputSize;
+    TypeParam inf = std::numeric_limits<TypeParam>::infinity();
+
+    TypeParam A[shapeA0][shapeA1] = {
+        {1.0, 2.0},
+        {inf, 1.0},
+        {inf, inf},
+        {-inf,inf},
+        {0.0, NAN},
+        {inf, NAN}
+    };
+    TypeParam B[shapeB];
+    this->calcShape({shapeA0,shapeA1},axis,&m,&n,&k,&outputSize);
+    
+    ASSERT_EQ(shapeA0,  m);
+    ASSERT_EQ(shapeA1,  n);
+    ASSERT_EQ(1,        k);
+    ASSERT_EQ(shapeB,   outputSize);
+
+    this->test_matlib_reducemax(
+        m,n,k,
+        (TypeParam*)A,
+        (TypeParam*)B
+    );
+
+    EXPECT_EQ(2,       B[0]);
+    EXPECT_TRUE(inf==  B[1]);
+    EXPECT_TRUE(inf==  B[2]);
+    EXPECT_TRUE(inf==  B[3]);
+    EXPECT_TRUE(std::isnan(B[4]));
+    EXPECT_TRUE(std::isnan(B[5]));
 }
 
 }
