039eefaf9a1124c889b10a90726be5b73c60d451
Yuichi Ishikawa <yuichiis@users.noreply.github.com>
2024-05-21 18:09:28 +0900

    t

--- threadtest/SumThreadTest.cpp
+++ threadtest/SumThreadTest.cpp
@@ -71,18 +71,18 @@ TYPED_TEST(SumThreadTest, normal) {
     TypeParam X[N] = {1,2,-3,-4,5,-6};
     //TypeParam inf = std::numeric_limits<TypeParam>::infinity();
 
-    int32_t dtype = this->test_get_dtype(X[0]);
-    if(dtype==rindow_matlib_dtype_int32) {
-        int64_t sum = (int64_t)this->test_matlib_sum(N, X, incX);
-        EXPECT_EQ((1+2-3-4+5-6), sum);
-    } else {
+    //int32_t dtype = this->test_get_dtype(X[0]);
+    //if(dtype==rindow_matlib_dtype_int32) {
+    //    int64_t sum = (int64_t)this->test_matlib_sum(N, X, incX);
+    //    EXPECT_EQ((1+2-3-4+5-6), sum);
+    //} else {
         TypeParam sum = (TypeParam)this->test_matlib_sum(N, X, incX);
         EXPECT_EQ((1+2-3-4+5-6), sum);
-    }
+    //}
 
     // bool
-    bool boolX[N] = {true,false,true,false,true,false};
-    int64_t sumbool = this->test_matlib_sum(N, boolX, incX);
-    EXPECT_EQ(3, sumbool);
+    //bool boolX[N] = {true,false,true,false,true,false};
+    //int64_t sumbool = this->test_matlib_sum(N, boolX, incX);
+    //EXPECT_EQ(3, sumbool);
 }
 }
